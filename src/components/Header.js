836ac369124dda04555326cfe3517f64233ec9f9
Tom Rodrigues <tomjrodrigues@gmail.com>
2019-02-11 18:03:38 -0800

    text edits

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,15 +10,7 @@ class Header extends React.Component {
                 <div className="inner">
                     <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
                     <h1><strong>I am Tom Rodrigues</strong>, a Portland-<br />
-<<<<<<< HEAD
-<<<<<<< HEAD
                     based Web Developer.</h1>
-=======
-                    based Front End Developer.</h1>
->>>>>>> parent of 36e0f0e0... Updates
-=======
-                    based Front End Developer.</h1>
->>>>>>> parent of 36e0f0e0... Updates
                 </div>
                 <Footer />
             </header>
