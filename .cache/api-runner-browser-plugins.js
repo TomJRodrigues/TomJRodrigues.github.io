0c6f13c27c3b58b2deb769a2a38d9ef58a508ef9
Tom Rodrigues <tomjrodrigues@gmail.com>
2019-02-11 18:15:49 -0800

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/Tom/programming/websites/TomJRodrigues.github.io/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/Tom/programming/websites/TomJRodrigues.github.io/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
